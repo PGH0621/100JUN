1f63e9aeba944dad4b91e7cd855dec68c7aab9d6
GH Park <rmsgh1889@naver.com>
2025-03-31 17:12:59 +0900

    2941 silver5

--- C/100JUN.c
+++ C/100JUN.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#if 1 //1057 silver4 토너먼트
+#if 0 //1057 silver4 토너먼트
 #include <stdio.h>
 
 int main() {
@@ -17,5 +17,24 @@ int main() {
 
     printf("%d\n", round);
     return 0;
+}
+#endif
+
+#if 1 //2941 silver5 크로아티아 알파벳  
+int main() {
+	char str[101] = { 0 };
+	scanf("%s", str);
+	int cnt = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'c' && (str[i + 1] == '=' || str[i + 1] == '-')) i++;
+        else if (str[i] == 'd') {
+            if (str[i + 1] == 'z' && str[i + 2] == '=') i += 2;
+            else if (str[i + 1] == '-') i++;
+        }
+        else if ((str[i] == 'l' || str[i] == 'n') && str[i + 1] == 'j') i++;
+        else if ((str[i] == 's' || str[i] == 'z') && str[i + 1] == '=') i++;
+        cnt++;
+    }
+	printf("%d", cnt);
 }
 #endif
