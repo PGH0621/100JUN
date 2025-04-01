ed37dbed3f537c993970df9b2c8234d5e963d472
GH Park <rmsgh1889@naver.com>
2025-04-01 16:56:05 +0900

    2444 별찍기 7

--- C/100JUN.c
+++ C/100JUN.c
@@ -19,8 +19,7 @@ int main() {
     return 0;
 }
 #endif
-
-#if 1 //2941 silver5 크로아티아 알파벳  
+#if 0 //2941 silver5 크로아티아 알파벳  
 int main() {
 	char str[101] = { 0 };
 	scanf("%s", str);
@@ -37,4 +36,78 @@ int main() {
     }
 	printf("%d", cnt);
 }
+#endif
+#if 0 //25083 bronze5 새싹
+int main() {
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return 0;
+}
+#endif
+#if 0 //3003 bronze5 킹, 퀸, 룩, 비숍, 나이트, 폰
+#include <stdio.h>
+int main() {
+    int arr[6] = { 1, 1, 2, 2, 2, 8 };
+    int input[6] = { 0 };
+    for (int i = 0; i < 6; i++) {
+        scanf("%d", &input[i]);
+        printf("%d ", arr[i] - input[i]);
+    }
+    return 0;
+}
+
+#endif
+#if 0 //2444 bronze3 별 찍기 - 7
+#include <stdio.h>
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int k = 0; k < n - i; k++)
+			printf(" ");
+		for (int k = 0; k < i + (i - 1); k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for (int i = n - 1; i > 0; i--) {
+		for (int k = 0; k < n - i; k++)
+			printf(" ");
+		for (int k = 0; k < i + (i - 1); k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
+#endif
+#if 1 //1016 gold1 제곱 ㄴㄴ
+int main(){
+    long long a, b;
+    long long cnt = 0;  
+    long long arr[1000001] = { 0 };
+    scanf("%lld %lld", &a, &b);     
+    for (long long i = 2; i * i <= b; i++){
+        long long start = a / (i * i) * (i * i);
+        if (start < a) start += (i * i);
+        for (long long j = start; j <= b; j += (i * i)){
+            arr[j - a] = 1;
+        }
+    }
+    for (long long i = 0; i <= b - a; i++){
+        if (arr[i] == 0) cnt++;
+    }
+    printf("%lld", cnt);
+    return 0;z
+}   
+
 #endif
