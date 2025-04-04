ea850f9865f55a151a25fa2e531da6a4ed0fafa0
GH Park <rmsgh1889@naver.com>
2025-04-04 20:39:48 +0900

    0404_2

--- C/100JUN.c
+++ C/100JUN.c
@@ -19,8 +19,7 @@ int main() {
     return 0;
 }
 #endif
-
-#if 1 //2941 silver5 크로아티아 알파벳  
+#if 0 //2941 silver5 크로아티아 알파벳  
 int main() {
 	char str[101] = { 0 };
 	scanf("%s", str);
@@ -37,4 +36,65 @@ int main() {
     }
 	printf("%d", cnt);
 }
+#endif
+#if 0 //1157 bronze1 단어 공부
+int main(){
+    int arr[26] = { 0 };
+
+    char str[1000001] = { 0 };
+    scanf("%s", str);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            arr[str[i] - 'a']++;
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+            arr[str[i] - 'A']++;
+        }
+    }
+    int max = 0;
+    int maxIndex = 0;
+    int flag = 0;
+    for (int i = 0; i < 26; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            maxIndex = i;
+            flag = 0;
+        }
+        else if (arr[i] == max) {
+            flag = 1;
+        }
+    }
+    if (flag == 1) {
+        printf("?\n");
+    }
+    else {
+        printf("%c\n", maxIndex + 'A');
+    }
+    return 0; 
+
+}
+#endif
+#if 1 //2839 silver4 설탕 배달
+int main() {
+    int N;
+    scanf("%d", &N);
+    int cnt = 0;
+    while (N > 0) {
+        if (N % 5 == 0) {
+            cnt += N / 5;
+            N = 0;
+        }
+        else {
+            N -= 3;
+            cnt++;
+        }
+    }
+    if (N < 0) {
+        printf("-1\n");
+    }
+    else {
+        printf("%d\n", cnt);
+    }
+}
+
 #endif
